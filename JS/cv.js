e2616621d977c2c85901d387cb6321e3705ed7bd
Asta296-png <asta.mbathie@univ-thies.sn>
2025-11-05 01:24:47 +0000

    Add typeText function for animated text display
    Ajout du fichier cv.js pour l’animation mot à mot

--- JS/cv.js
+++ JS/cv.js
@@ -1,1 +1,26 @@
 
+
+function typeText(id, text, speed) {
+  const el = document.getElementById(id);
+  let i = 0;
+  el.textContent = "";
+
+  function type() {
+    if (i < text.length) {
+      el.textContent += text.charAt(i);
+      i++;
+      setTimeout(type, speed);
+    }
+  }
+
+  type();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  typeText("typed-name", "Asta Mbathie", 150);
+  setTimeout(() => {
+    typeText("typed-title", "Étudiante en informatique", 150);
+  }, 2000);
+});
+
+
